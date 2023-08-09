9b70f7cd3a38ab285e413fb7a39974a025605547
helvetios16 <139891847+helvetios16@users.noreply.github.com>
2023-08-09 12:03:40 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <SFML/Graphics.hpp>
 #include <memory>
 #include "game.h"
+
+Games* Games::instance = nullptr;
+
 int main() {
     std::srand(static_cast<unsigned>(time(0)));
+    Games* instance1 = Games::getInstance();
 
-    std::unique_ptr<Games> game = std::make_unique<Games>();
-    game->run();
+    //std::unique_ptr<Games> game = std::make_unique<Games>();
+    instance1->run();
 
     return 0;
 }
